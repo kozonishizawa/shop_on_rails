70e863b2333132faadec789d3fd221fdaf53a063
西澤　康造 <Kozo@mba.local>
2019-10-03 13:10:56 +0900

    fixed

--- db/migrate/20191001155143_modify_cart_item_table.rb
+++ db/migrate/20191001155143_modify_cart_item_table.rb
@@ -1,10 +1,13 @@
 class ModifyCartItemTable < ActiveRecord::Migration[5.2]
   def change
 
+    remove_foreign_key :cart_items, :product_id
+    remove_foreign_key :ordered_items, :product_id
+
     rename_column :cart_items, :product_id, :product_stock_id
     rename_column :ordered_items, :product_id, :product_stock_id
 
-    #add_foreign_key :cart_items, :product_stocks, column: :product_stock_id
-    #add_foreign_key :ordered_items, :product_stocks, column: :product_stock_id
+    add_foreign_key :cart_items, :product_stocks, column: :product_stock_id
+    add_foreign_key :ordered_items, :product_stocks, column: :product_stock_id
   end
 end
